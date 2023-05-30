38c8b0688632b0334af40865d9f59e2993569978
zl837413707 <zl837413707@gmail.com>
2023-05-30 22:17:47 +0900

    完了画面

---
+++ src/views/myPayend.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="seat">
+    <SeatNav :process="getActive"></SeatNav>
+    <div class="seatContent">
+      <p>ご購入ありがとうございました。</p>
+      <p>ご注文確認メールをご登録いただきましたメールアドレスに送りいらしましたので、ご確認ください。</p>
+      <p>メールが届かない場合は、マイページのご注文履歴から状況を確認いただくことが可能です。</p>
+      <div class="order"><span class="left">注文番号:</span><span class="right">230524851032</span></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SeatNav from '../components/mySeatNav.vue'
+export default {
+  name: 'myPayend',
+  created() {
+    this.$store.commit('activeChange', 3)
+  },
+  computed: {
+    getActive() {
+      return this.$store.state.tab.active
+    }
+  },
+  components: {
+    SeatNav
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.seat {
+  margin: 0 auto;
+  width: 80%;
+  padding-bottom: 50px;
+
+  // background-color: pink;
+  .seatContentTop {
+    font-size: 25px;
+    font-weight: 400;
+    margin-left: 15px;
+  }
+
+  .seatContent {
+    margin: 0 auto;
+    width: 90%;
+    margin-top: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    p {
+      margin-bottom: 10px;
+    }
+
+    .order {
+      margin-top: 20px;
+
+      .left {
+        font-size: 30px;
+        font-weight: 700;
+        margin-right: 20px;
+
+      }
+
+      .right {
+        color: rgb(176, 5, 5);
+        font-weight: 700;
+      }
+    }
+
+  }
+}
+</style>
