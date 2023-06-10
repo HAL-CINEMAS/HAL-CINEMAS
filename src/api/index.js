ce6a2608cef66f7d2e8857faf4438401aed7c29f
Frogman1312 <kaek33586@gmail.com>
2023-06-10 11:45:01 +0900

    add firebase setting

--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,13 @@
-// import http from '../utils/request.js'
-
-// export const getMovieList = () => {
-//   return http.get('https://www.fastmock.site/mock/7295ac461fe075c8f9768284084b85a9/api/movieList')
-// }
-
 import app from './firebase.js'
 import { getFirestore, collection, getDocs } from 'firebase/firestore'
 
 const db = getFirestore(app)
+const datalist = []
 async function fetchData() {
-  let datalist = []
   const querySnapshot = await getDocs(collection(db, 'movie'))
   querySnapshot.forEach((doc) => {
-    datalist = doc.data().data
-    const msdata = datalist.seconds * 1000
+    const data = doc.data().data
+    const msdata = data.seconds * 1000
     const date = new Date(msdata)
     const year = date.getFullYear()
     const month = date.getMonth() + 1
