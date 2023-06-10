7a37dc5596a228f30e43f033cb16342f3066fbf9
Frogman1312 <kaek33586@gmail.com>
2023-06-10 11:45:19 +0900

    add firestore

--- src/views/register/myRegisterStep2.vue
+++ src/views/register/myRegisterStep2.vue
@@ -6,26 +6,30 @@
           <i :class="getIconClass(item.icon)" class="icon"></i>
           {{ item.name }}
         </template>
-        <span class="content">{{ reigisterInfo[item.user] }}</span>
+        <span class="content">{{ registerInfo[item.user] }}</span>
       </el-descriptions-item>
     </el-descriptions>
     <div class="submit">
       <router-link to="/register/step1" style="text-decoration: none;">
         <div class="left">戻る</div>
       </router-link>
-      <router-link to="/register/step3" style="text-decoration: none;">
+      <div @click='adduser' style="text-decoration: none;">
         <div class="right">確認</div>
-      </router-link>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import app from '@/api/firebase.js'
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+import { getFirestore, doc, setDoc, serverTimestamp, Timestamp } from 'firebase/firestore'
+
 export default {
   data() {
     return {
       title: [{ name: 'メールアドレス', icon: 'youxiang', user: 'username' }, { name: 'ユーザー名', icon: 'ziyuanxhdpi', user: 'name' }, { name: '氏名', icon: 'zuoxixingming', user: 'mail' }, { name: '性別', icon: 'xingbie', user: 'sex' }, { name: '生年月日', icon: 'calendar', user: 'birth' }, { name: '都道府県', icon: 'dingwei', user: 'address' }, { name: '電話番号', icon: 'shouji', user: 'phone' }],
-      reigisterInfo: []
+      registerInfo: []
     }
   },
   methods: {
@@ -37,8 +41,35 @@ export default {
       const oldArr = JSON.parse(localStorage.getItem('register'))
       const birth = `${oldArr.year}-${oldArr.month}-${oldArr.day}`
       const newArr = { ...oldArr, birth: birth }
-      this.reigisterInfo = newArr
-      console.log(this.reigisterInfo)
+      this.registerInfo = newArr
+    },
+
+    adduser() {
+      const data = this.registerInfo
+      let auth = getAuth(app)
+      const db = getFirestore(app)
+      auth = getAuth()
+      createUserWithEmailAndPassword(auth, data.mail, 'this.registerInfo.pass').then((userCredential) => {
+        setDoc(doc(db, 'user', userCredential.user.uid), {
+          name: data.username,
+          phone: data.phone,
+          birth: Timestamp.fromDate(new Date(data.year, data.month - 1, data.day)),
+          address: data.address,
+          sex: data.sex,
+          timestamp: serverTimestamp()
+        }).catch((error) => {
+          const errorCode = error.code
+          const errorMessage = error.message
+          console.log(errorCode)
+          console.log(errorMessage)
+        })
+        this.$router.push('/register/step3')
+      }).catch((error) => {
+        const errorCode = error.code
+        const errorMessage = error.message
+        console.log(errorCode)
+        console.log(errorMessage)
+      })
     }
   },
   created() {
