ab79e7de19e74a66bf14eb83b7a2cf1f411cf9b3
zl837413707 <zl837413707@gmail.com>
2023-06-30 09:28:26 +0900

    noraml

--- src/views/mySchedule.vue
+++ src/views/mySchedule.vue
@@ -220,6 +220,7 @@ export default {
     changeColor(item, index) {
       this.activeIndex = index
       this.selectedDate = item
+      // console.log(this.movieContent)
     }
   },
   created() {
